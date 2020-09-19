f2e1bfb67fd5d19947c1c2fb71b65f4941945f32
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-09-19 13:16:54 +0530

    Adding blog posts to database

--- src/java/com/tech/blog/dao/PostDao.java
+++ src/java/com/tech/blog/dao/PostDao.java
@@ -1,6 +1,7 @@
 
 package com.tech.blog.dao;
 import com.tech.blog.entities.Category;
+import com.tech.blog.entities.Post;
 import java.sql.*;
 import java.util.ArrayList;
 public class PostDao {
@@ -31,5 +32,26 @@ public class PostDao {
             e.printStackTrace();
         }
         return list;
+    }
+    public boolean savePost(Post p)
+    {
+        boolean f=false;
+        try {
+            String q="insert into posts(pTitle, pContent, pCode, pPic, catId, userId) values(?,?,?,?,?,?)";
+            PreparedStatement pstmt=con.prepareStatement(q);
+            pstmt.setString(1, p.getpTitle());
+            pstmt.setString(2, p.getpContent());
+            pstmt.setString(3, p.getpCode());
+            pstmt.setString(4, p.getpPic());
+            pstmt.setInt(5, p.getCatId());
+            pstmt.setInt(6, p.getUserId());
+            
+            pstmt.executeUpdate();
+            f=true;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
     }
 }
